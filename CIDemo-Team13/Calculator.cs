9fd86154d3e3658f454596830f28006baf3b0632
Mikkel <mikk203i@gmail.com>
2025-04-10 10:15:59 +0200

    Tilføjet Calculator.cs, ICalculator.cs og xUniTest.cs

---
+++ CIDemo-Team13/Calculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIDemo_Team13
+{
+    public class Calculator : ICalculator
+    {
+        public int Add(int a, int b)
+        {
+            checked // For at håndtere overflow
+            {
+                return a + b;
+            }
+        }
+
+        public int Subtract(int a, int b)
+        {
+            checked // For at håndtere overflow
+            {
+                return a - b;
+            }
+        }
+
+        public int Multiply(int a, int b)
+        {
+            checked // For at håndtere overflow
+            {
+                return a * b;
+            }
+        }
+
+        public int Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Division med nul er ikke tilladt");
+            }
+
+            return a / b;
+        }
+    }
+}
